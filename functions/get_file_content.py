502bf02b990c5c313921bc16c9809a8ae04afe94
lavelius <jonathansirmans29@gmail.com>
2025-07-01 17:05:32 -0700

    more functions and tests completed

---
+++ functions/get_file_content.py
@@ -0,0 +1,23 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    """
+    Get the content of a file in the specified working directory.
+    """
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    # Ensure abs_file_path is within working_directory
+    if os.path.commonpath([abs_file_path, abs_working_directory]) != abs_working_directory:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    with open(abs_file_path, 'r') as file:
+        content = file.read()
+    
+    if len(content) > 10000:
+        content = content[:10000] + f'...File "{file_path}" truncated at 10000 characters'
+
+    return content
--- tests.py
+++ tests.py
@@ -1,16 +1,17 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def main():
-    files_info = get_files_info("calculator", ".")
+    files_info = get_file_content("calculator", "lorem.txt")
     print(files_info)
 
-    files_info = get_files_info("calculator", "pkg")
+    files_info = get_file_content("calculator", "main.py")
     print(files_info)
 
-    files_info = get_files_info("calculator", "/bin")
+    files_info = get_file_content("calculator", "pkg/calculator.py")
     print(files_info)
 
-    files_info = get_files_info("calculator", "../")
+    files_info = get_file_content("calculator", "/bin/cat")
     print(files_info)
 
 
